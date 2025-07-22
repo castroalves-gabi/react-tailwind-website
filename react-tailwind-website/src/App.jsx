90d6925e633901f64e2b320d468a90ebfc6cf0c1
castroalves-gabi <ile.bgk@gmail.com>
2025-07-22 15:46:53 -0300

    second commit

---
+++ react-tailwind-website/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-600">
+      Tailwind funcionando 🎉
+    </div>
+  );
+}
+
+export default App;
